9730c1f183709ec117f1e3b8329853a85f7fa9ad
Lars Fronius <l.fronius@googlemail.com>
2013-07-30 17:22:25 +0200

    php54 is now in the oldstable location

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,7 +2,7 @@ class php::repo {
 
   if $php::params::repository == 'ondrej-php54' {
     apt::source { 'ondrej-php54':
-      location => 'http://ppa.launchpad.net/ondrej/php5/ubuntu',
+      location => 'http://ppa.launchpad.net/ondrej/php5-oldstable/ubuntu',
       key      => 'E5267A6C',
       release  => 'precise',
       repos    => 'main'
